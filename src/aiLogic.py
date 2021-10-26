f485d50d6a614f3d3b239ace2e674c60d948392a
ElComadore <coen.olofsson@gmail.com>
2021-10-26 19:53:15 +0200

    Updated all of the comments and function descriptions

--- src/aiLogic.py
+++ src/aiLogic.py
@@ -2,7 +2,16 @@ import numpy as np
 
 
 class AIPlayer:
+    """
+    The AI that is to learn how to play the game through guessing its way forward
+    """
     def __init__(self, name: str, token: str, states=None):
+        """
+        Set up our beautiful AI boi
+        :param name: name just for tracking
+        :param token: what symbol we are playing with, will be used for parsing later
+        :param states: do we already have some states and probabilities to work with?
+        """
         if states is None:
             states = list()
         self.states = states
@@ -11,55 +20,78 @@ class AIPlayer:
         self.played_moves = list()
 
     def __add_state(self, parsed):
-        possibilities = self.__get_available_moves(parsed)
-        probabilities = self.__set_probabilities(possibilities)
-        frequencies = self.__set_frequencies(possibilities)
-        self.states.append(dict(state=parsed,
+        """
+        Add a new game state to memory along with setting up its initial probabilities
+        :param parsed: the parsed game state
+        :return: nothing
+        """
+        possibilities = self.__get_available_moves(parsed)          # What moves are legal?
+        probabilities = self.__set_probabilities(possibilities)     # What should their probabilities be?
+        frequencies = self.__set_frequencies(possibilities)         # What should their frequencies be?
+        self.states.append(dict(state=parsed,                       # Each state is a dictionary of these things!
                                 possibilities=possibilities,
                                 probabilities=probabilities,
                                 frequencies=frequencies))
 
     def __parse_state(self, state):
-        parsed = [[None for y in range(len(state[0]))] for x in range(len(state))]
+        """
+        Change the board with symbols into a board which is made of 0s and 1s so that we can learn regardless of which
+        symbol we decide to give the AI, so long as we give 2 different symbols
+        :param state: present unadulterated game state
+        :return: the parsed state
+        """
+        parsed = [[None for y in range(len(state[0]))] for x in range(len(state))]      # Initial parsed board
 
         for i in range(len(state)):
             for j in range(len(state[0])):
-                if state[i][j] is None:
+                if state[i][j] is None:             # If square not used do nothing
                     continue
-                elif state[i][j] == self.token:
+                elif state[i][j] == self.token:     # If we have a token there, set to 1
                     parsed[i][j] = 1
                 else:
-                    parsed[i][j] = 0
+                    parsed[i][j] = 0                # If someone else has a token there, set to 0
         return parsed
 
     def get_move(self, state):
-        parsed = self.__parse_state(state)
-        move_vector = self.__get_state(parsed)
+        """
+        What move is the AI going to play given the current board state?
+        :param state: the current unadulterated board state
+        :return: an index on the board
+        """
+        parsed = self.__parse_state(state)          # Set the parsed state
+        move_vector = self.__get_state(parsed)      # Get the possibilities and probabilities for current game state
 
-        random = np.random.uniform(0, 1)
+        random = np.random.uniform(0, 1)            # Generate a random choice0
 
         i = 0
         s = move_vector['probabilities'][i]
 
-        while s < random:
+        while s < random:       # Check which move in the move_vector we are going to play
             i += 1
             try:
                 s = s + move_vector['probabilities'][i]
             except IndexError:
                 i -= 1
                 break
-        self.played_moves.append(dict(move_vector=move_vector, played=i))
+        self.played_moves.append(dict(move_vector=move_vector, played=i))       # Add move to played moves for later
         return move_vector['possibilities'][i]
 
     def reinforce_probabilities(self):
+        """Generate the new probabilties given the observed frequencies; this needs tweaking"""
         for state in self.states:
             s = np.sum(state['frequencies'])
 
             for i in range(len(state['probabilities'])):
-                state['probabilities'][i] = (state['frequencies'][i]/s + state['probabilities'][i])/2
+                state['probabilities'][i] = (state['frequencies'][i]/s + state['probabilities'][i])/2   # Just average
                 state['frequencies'][i] = 1
 
     def reinforce_frequencies(self, score=0):
+        """
+        Adding frequencies of wins for each move; the score reflects how much we want to reward a winning series of
+        moves
+        :param score: how much do we want to reward winning moves
+        :return:
+        """
         for move in self.played_moves:
             played = move['played']
 
@@ -68,6 +100,12 @@ class AIPlayer:
         self.played_moves = list()
 
     def __get_state(self, state):
+        """
+        Gets the current state from the AIs played game states, adds one if not in the list.
+        We need a much better structure for this because having a list is a nightmare; search tree?
+        :param state:
+        :return:
+        """
         num_checked = 0
         broken = False
 
@@ -75,7 +113,7 @@ class AIPlayer:
             is_match = 0
             for i in range(len(state)):
                 for j in range(len(state[0])):
-                    if s['state'][i][j] != state[i][j]:
+                    if s['state'][i][j] != state[i][j]:     # Just garbage
                         num_checked += 1
                         broken = True
                         break
@@ -88,12 +126,17 @@ class AIPlayer:
             if is_match == 9:
                 return s
 
-        if num_checked >= len(self.states):
+        if num_checked >= len(self.states):     # Adding a new state and returning it
             self.__add_state(state)
             return self.states[-1]
 
     @staticmethod
     def __get_available_moves(state):
+        """
+        What positions are blank?
+        :param state: a game state - either parsed or not
+        :return: a list of available position indexes
+        """
         available = list()
         for i in range(len(state)):
             for j in range(len(state[0])):
@@ -104,6 +147,11 @@ class AIPlayer:
 
     @staticmethod
     def __set_probabilities(possibilities):
+        """
+        Takes a possibility list and returns a uniform distribution for them
+        :param possibilities: list of available moves
+        :return: a list of probabilities
+        """
         probabilities = list()
         for i in range(1, len(possibilities) + 1):
             probabilities.append(np.divide(1, len(possibilities)))
@@ -111,6 +159,11 @@ class AIPlayer:
 
     @staticmethod
     def __set_frequencies(possibilities):
+        """
+        Sets initial frequencies for a possibility vector, could have literally just use numpy
+        :param possibilities: list of possible moves
+        :return: a list of 1s
+        """
         frequencies = list()
         for i in range(len(possibilities)):
             frequencies.append(1)
--- src/gameLogic.py
+++ src/gameLogic.py
@@ -3,40 +3,54 @@ import pickle
 
 
 class TicTacToe:
+    """
+    The actual game of naughts and crosses, nothing really strange here, just modified slightly to incorporate the
+    reinforcement directly.
+    """
     def __init__(self):
-        self.board = [[None for x in range(3)] for y in range(3)]
-        self.players = list()
-        self.num_wins = [0, 0, 0]
-        self.player_number = 0
+        self.board = [[None for x in range(3)] for y in range(3)]       # Play space
+        self.players = list()                                           # Who's playing
+        self.num_wins = [0, 0, 0]                                       # A win record
+        self.player_number = 0                                          # Who is playing
 
     def play(self, p: bool):
+        """
+        The central play loop
+        :param p: do we print the board?
+        :return: who won?
+        """
         self.player_number = 0
         while True:
-            cur_player = self.players[self.player_number]
-            player_move = cur_player.get_move(self.board)
-            self.board[player_move[0]][player_move[1]] = cur_player.token
+            cur_player = self.players[self.player_number]                       # Get current player
+            player_move = cur_player.get_move(self.board)                       # Get player move
+            self.board[player_move[0]][player_move[1]] = cur_player.token       # Set the move
 
-            if p:
+            if p:                       # Do we print the board?
                 for b in self.board:
                     print(b)
                 print('\n')
 
-            if self.player_win(player_move):
-                cur_player.reinforce_frequencies(score=3)
-                self.players[(self.player_number + 1) % 2].reinforce_frequencies()
-                self.board = [[None for x in range(3)] for y in range(3)]
-                self.num_wins[self.player_number] += 1
+            if self.player_win(player_move):                                        # Check if win
+                cur_player.reinforce_frequencies(score=10)                          # Adding frequencies with some score
+                self.players[(self.player_number + 1) % 2].reinforce_frequencies()  # Adding frequencies with 0 zero
+                self.board = [[None for x in range(3)] for y in range(3)]           # Reset board
+                self.num_wins[self.player_number] += 1                              # Record who won
                 return cur_player.name
-            elif self.tie():
-                cur_player.reinforce_frequencies(score=1)
+            elif self.tie():                                                                # Check for tie
+                cur_player.reinforce_frequencies(score=1)                                   # Just say this wasn't bad
                 self.players[(self.player_number + 1) % 2].reinforce_frequencies(score=1)
-                self.board = [[None for x in range(3)] for y in range(3)]
-                self.num_wins[2] += 1
+                self.board = [[None for x in range(3)] for y in range(3)]           # Reset board
+                self.num_wins[2] += 1                                               # Record a tie
                 return "no one!"
-            else:
+            else:                                                                   # Next player's turn
                 self.player_number = (self.player_number + 1) % 2
 
     def player_win(self, last_played):
+        """
+        Did the last move win the game? Uses some sussy fucking logic that is not scalable at all, at all
+        :param last_played: the index of the last played move
+        :return: True if game was won, False otherwise
+        """
 
         a = self.board[last_played[0]][0]
         b = self.board[0][last_played[1]]
@@ -63,7 +77,7 @@ class TicTacToe:
             pass
         return False
 
-    def tie(self):
+    def tie(self):              # Check if board is full
         for b in self.board:
             if None in b:
                 return False
@@ -77,16 +91,21 @@ leo = aiLogic.AIPlayer('leo', 'o')
 game.players.append(david)
 game.players.append(leo)
 
-for i in range(50000):
-    game.play(p=False)
-    if i % 1000 == 999:
-        david.reinforce_probabilities()
-        leo.reinforce_probabilities()
-        print('Reinforcing:' + str((i+1)/1000))
+n = 5000                                # How many games do we play before we change the probabilities?
 
-print('Game over!')
-print(game.num_wins)
+for i in range(150*n):                  # Scalar reflects how many total games we are playing
+    game.play(p=False)
+    if i % n == n - 1:
+        david.reinforce_probabilities()         # Derive new probabilities
+        leo.reinforce_probabilities()
+        print('Reinforcing:' + str((i+1)/n))    # Which reinforce are we on
+        print(game.num_wins)
+        game.num_wins = [0, 0, 0]               # Just so we can see how the win-rates are progressing
 
 for i in range(5):
+    print('----- New Game -----\n')
     game.play(True)
-print('xd')
+
+print(david.states[0])
+print('Game over!')
+print(game.num_wins)
